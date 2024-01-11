1e7ec5b283a3b4f73130769a77e60d1e9a3d0ec7
MarcusQuirino <marcusfilipus@gmail.com>
2024-01-11 17:31:02 -0300

    :3

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 function App() {
   return (
     <>
-      <h1 className="text-red-500">tutorial react</h1>
+      <h1 className='text-red-500'>tutorial react</h1>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
